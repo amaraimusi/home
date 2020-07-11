9c6f83df599599e061fb14c0c7a70e0354043c8b
user <amaraimusi@gmail.com>
2020-07-11 11:08:46 +0900

    便利ツール：ハードウェアの情報システムツールなら、 CPU-ZやSpeccy

---
+++ 01_upload.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/sh
+echo 'ソースコードを差分アップロードします。'
+
+rsync -auvz ../home amaraimusi@amaraimusi.sakura.ne.jp:www
+rsync -auvz index.html amaraimusi@amaraimusi.sakura.ne.jp:www
+
+
+echo "------------ 送信完了"
+cmd /k
